933bfc62d5f6fa92d3eb4430050c211060781c0a
Maedeh Masoumi <matimasoomi67@gmail.com>
2024-10-09 18:23:40 +0330

    init

---
+++ task.py
@@ -0,0 +1,23 @@
+import datetime
+class task:
+    def __init__(self,title,description,task_id):
+        self.title=title
+        self.description=description
+        self.task_id=str(task_id)
+        if not self.task_id.isdigit():
+            raise ValueError('should digit')
+        self.start_time=datetime.datetime.now()
+        self.end_time=None
+        self.status=False
+
+    def mark_as_done(self):
+        self.status=True
+        self.end_time=datetime.datetime.now()
+    def __str__(self):
+        return f'ID:{self.task_id},Title:{self.title},
+        Description:{self.description},'\
+        f'start Time:{self.start_time},end Time:{self.end_time},
+        status:{self.status}'
+              
+        
+
